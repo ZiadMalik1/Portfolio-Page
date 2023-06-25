c3d4baca3bdd6a478cf74aab7d42ad559c066b4b
Ziad <zmali002@fiu.edu>
2023-06-24 20:00:50 -0400

    Added Testimonials

--- src/components/testimonials/Data.jsx
+++ src/components/testimonials/Data.jsx
@@ -1,6 +1,9 @@
 import Image1 from "../../assets/testimonial1.jpeg";
 import Image2 from "../../assets/Shiv.jpeg";
 import Image3 from "../../assets/Umut.jpeg";
+import Image4 from "../../assets/sidd.jpg";
+import Image5 from "../../assets/hayden.jpg";
+
 
 export const Data = [
   {
@@ -13,16 +16,34 @@ export const Data = [
   },
   {
     id: 2,
-    image: Image2,
-    title: "Shivanni Jagessar",
-    position: "Assistant Teaching Professor at F.I.U",
-    description: "Ziad has been my teaching assistant for the past two years (2019-2021). He has been the most outstanding teaching assistant that I have worked with. Ziad is very organized and manages his duties responsibly. He has a genuine eagerness and desire to help students and is sincerely interested in their progress"
+    image: Image4,
+    title: "Siddhartha Ghosh",
+    position: "Manager of Software Engineering at Mastercard",
+    description:
+    "Ziad was an excellent software engineering intern in our team. His attention to details, can-do attitude and curious mind would definitely help him to thrive in his career. Best of luck!",
+  },
+  {
+    id: 3,
+    image: Image5,
+    title: "Hayden Haddock",
+    position: "Senior Software Engineer at Mastercard",
+    description:
+    "What an energetic and driven young professional. Over the summer of 2022 I worked along side Ziad as a software engineer. From the time we met, Ziad was engaged and all in on every task he was given. Ziad’s ability to problem solve and ask intelligent questions got him working on defects within his first week on the team. He was a sponge and soaked up a lot of information on the software development lifecycle, and was involved deeply in all aspects of development. During his time with the team he built a micro service by himself and created an end to end implementation for how to integrate it with our platform. It was so refreshing to be around him during his internship. Ziad’s naturally curious mind and ability to quickly understand and apply his knowledge in practice will make him an awesome addition to any engineering team. All the best in the future I can’t wait to see where the future takes him!",
   },
   {
     id: 2,
+    image: Image2,
+    title: "Shivanni Jagessar",
+    position: "Assistant Teaching Professor at F.I.U",
+    description:
+      "Ziad has been my teaching assistant for the past two years (2019-2021). He has been the most outstanding teaching assistant that I have worked with. Ziad is very organized and manages his duties responsibly. He has a genuine eagerness and desire to help students and is sincerely interested in their progress",
+  },
+  {
+    id: 3,
     image: Image3,
     title: "Umut Caglar",
     position: "Assistant Teaching Professor at F.I.U",
-    description: "Ziad has been my teaching assistant for almost a year (2020-2020). He really does a good job. He's always very organized and on time. I believe students also like him as he is really friendly and approachable"
-  }
+    description:
+      "Ziad has been my teaching assistant for almost a year (2020-2020). He really does a good job. He's always very organized and on time. I believe students also like him as he is really friendly and approachable",
+  },
 ];
